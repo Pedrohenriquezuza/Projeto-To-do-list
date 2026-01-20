bcfea2bb4a1a7771164d8c2e53cfccda758264e1
Pedrohenriquezuza <phzuza07@gmail.com>
2026-01-20 20:14:57 -0300

    Componente de container feito

---
+++ src/components/Container.tsx
@@ -0,0 +1,31 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+export const ContainerVariants = cva(`mx-auto`, {
+  variants: {
+    size: {
+      md: "max-w-126 px-2",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface ContainerProps
+  extends VariantProps<typeof ContainerVariants>, React.ComponentProps<"div"> {
+  as?: keyof React.JSX.IntrinsicElements;
+}
+
+export default function Container({
+  as = "div",
+  children,
+  className,
+  ...props
+}: ContainerProps) {
+  return React.createElement(
+    as,
+    { className: ContainerVariants({ size: "md", className }), ...props },
+    children,
+  );
+}
